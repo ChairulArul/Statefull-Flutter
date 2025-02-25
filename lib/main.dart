83e6c79c36d6cd87b4a225edbacde8f1cee00c61
Arul Chairul <chairul@students.mikroskil.ac.id>
2025-02-25 14:47:16 +0700

    latihan statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,73 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   int counter = 1; // Pindahkan ke dalam State agar tidak reset
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         backgroundColor: Colors.blueGrey,
+//         appBar: AppBar(
+//           title: Text("Menggunakan Stateful"),
+//           centerTitle: true,
+//         ),
+//         body: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               counter.toString(),
+//               style: TextStyle(
+//                 fontSize: 20 + double.parse(counter.toString()),
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.white,
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 // Button untuk tambah
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       counter++;
+//                     });
+//                   },
+//                   child: Icon(Icons.add),
+//                 ),
+
+//                 // Button untuk kurang
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       if (counter != 1) {
+//                         counter--;
+//                       }
+//                     });
+//                   },
+//                   child: Icon(Icons.remove),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,53 +82,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 1; // Pindahkan ke dalam State agar tidak reset
-
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("Menggunakan Stateful"),
+          title: Text("Latihan buat Statefull"),
           centerTitle: true,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(
-              counter.toString(),
-              style: TextStyle(
-                fontSize: 20 + double.parse(counter.toString()),
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+              "Angka saat ini: $counter",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                // Button untuk tambah
                 ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;
-                    });
-                  },
-                  child: Icon(Icons.add),
-                ),
-
-                // Button untuk kurang
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                    child: Icon(Icons.add)),
                 ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (counter != 1) {
+                    onPressed: () {
+                      setState(() {
                         counter--;
-                      }
-                    });
-                  },
-                  child: Icon(Icons.remove),
-                ),
+                      });
+                    },
+                    child: Icon(Icons.remove))
               ],
             )
           ],
